e4cf5e58d0a4160b19c32e1e2c9efe208c1595fb
adrigalle <adriana.go1105@gmail.com>
2022-07-23 23:34:08 -0700

    still navbar no work

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+fetch('navbar.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
